b2ecfadc41a61a014d1848f036f7e0a6667420b0
David Sirait <davidsirait1156@gmail.com>
2025-06-25 22:56:52 +0700

    update C++ bug

--- SRC_CPP/OOP_SRC/main.cpp
+++ SRC_CPP/OOP_SRC/main.cpp
@@ -9,32 +9,66 @@
 
 using namespace hLocation;
 
-int main(){
-    Location SV_IPB = Location("SV IPB", -6.588457, 106.806200);
-    Location Danau  = Location("Danau IPB", -6.559582, 106.726720);
-
-    SV_IPB.Printer();
-    Danau.Printer();
-
-    fmt::println("{}", Misc::PrintMid("Degree", '-'));
-    double DEG = Distance::Distance_Deg(SV_IPB, Danau);
+namespace Main{
+    void IPB(){
+        Location SV_IPB = Location("SV IPB", -6.588457, 106.806200);
+        Location Danau  = Location("Danau IPB", -6.559582, 106.726720);
     
-    SV_IPB.toRadian();
-    Danau.toRadian();
+        SV_IPB.Printer();
+        Danau.Printer();
     
-    fmt::println("{}", Misc::PrintMid("Radian", '-'));
-    double RAD = Distance::Distance_Rad(SV_IPB, Danau);
+        fmt::println("{}", Misc::PrintMid("Degree", '-'));
+        double DEG = Distance::Distance_Deg(SV_IPB, Danau);
+        
+        SV_IPB.toRadian();
+        Danau.toRadian();
+        
+        fmt::println("{}", Misc::PrintMid("Radian", '-'));
+        double RAD = Distance::Distance_Rad(SV_IPB, Danau);
+        
+        fmt::println("{}", fmt::format("{:~^{}}", "~", Misc::TerminalSize()));
+        fmt::println("Degrees = {} KM", DEG);
+        fmt::println("Radians = {} KM", RAD);
     
-    fmt::println("{}", fmt::format("{:~^{}}", "~", Misc::TerminalSize()));
-    fmt::println("Degrees = {} KM", DEG);
-    fmt::println("Radians = {} KM", RAD);
-
-    if(DEG == RAD){
-        fmt::println("APPROVED!");
-    } else {
-        fmt::println("meh");
+        if(DEG == RAD){
+            fmt::println("APPROVED!");
+        } else {
+            fmt::println("meh");
+        }
     }
 
-    return 0;
+    void WikipediaExample(){
+        Location WhiteHouse = Location("White House WDC", 38.898, 77.037);
+        Location EifflTower = Location("Effiel Tower à Paris", 48.858, 2.294);
+
+        WhiteHouse.Printer();
+        EifflTower.Printer();
+
+        fmt::println("{}", Misc::PrintMid("Degree", '-'));
+        double DEG = Distance::Distance_Deg(WhiteHouse, EifflTower);
+        
+        WhiteHouse.toRadian();
+        EifflTower.toRadian();
+        
+        fmt::println("{}", Misc::PrintMid("Radian", '-'));
+        double RAD = Distance::Distance_Rad(WhiteHouse, EifflTower);
+        
+        fmt::println("{}", fmt::format("{:~^{}}", "~", Misc::TerminalSize()));
+        fmt::println("Degrees = {} KM", DEG);
+        fmt::println("Radians = {} KM", RAD);
+    
+        if(DEG == RAD){
+            fmt::println("APPROVED!");
+        } else {
+            fmt::println("meh");
+        }
+    }
 }
 
+int main(){
+    fmt::println("{}", Misc::PrintMid("IPBs"));
+    Main::IPB();
+    
+    fmt::println("{}", Misc::PrintMid("Wikipedia Example"));
+    Main::WikipediaExample();
+}
